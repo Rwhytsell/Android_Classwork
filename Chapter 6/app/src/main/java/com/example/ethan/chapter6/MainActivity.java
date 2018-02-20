057e32f05fca45ffa490149d11a822ed5aa02147
Ryan <Rwhytsell67@gmail.com>
2018-02-20 15:07:21 -0500

    First layout and model

--- Chapter 6/app/src/main/java/com/example/ethan/chapter6/MainActivity.java
+++ Chapter 6/app/src/main/java/com/example/ethan/chapter6/MainActivity.java
@@ -2,12 +2,78 @@ package com.example.ethan.chapter6;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    UserData ud = new UserData();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        final TextView guests = findViewById(R.id.GuestsNumber);
+        final SeekBar guestslider = findViewById(R.id.GuestInput);
+
+        guests.setText(String.valueOf(guestslider.getProgress()));
+
+        guestslider.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                guests.setText(String.valueOf(i));
+                guestsChanged(i);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        final TextView tip = findViewById(R.id.TipNumber);
+        final SeekBar tipslider = findViewById(R.id.TipInput);
+
+        tip.setText(String.valueOf(tipslider.getProgress()));
+
+        tipslider.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                tip.setText(String.valueOf(i));
+                tipChanged(i);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    public void guestsChanged(int i){
+        ud.setGuests(i);
+        update();
+    }
+
+    public void tipChanged(int i){
+        ud.setTipPercentage((double) i);
+        update();
+    }
+
+    public void update(){
+        // TODO update all fields with values in UserData (ud)
     }
 }
---
+++ Chapter 6/app/src/main/java/com/example/ethan/chapter6/UserData.java
@@ -0,0 +1,118 @@
+package com.example.ethan.chapter6;
+
+/**
+ * Model used to hold user entered data and calculate other values from that data.
+ *
+ */
+public class UserData {
+    private int guests;
+    private double tipPercentage;
+    private double bill;
+    private double totalTip = bill * (tipPercentage / 100);
+    private double totalBill = bill + totalTip;
+    private double tipPerGuest = totalTip / guests;
+    private double totalPerGuest = totalBill / guests;
+
+    /**
+     * Instanciates the user data with the default values
+     * No parameters because it should only be created once
+     */
+    public UserData(){
+        this.guests = 2;
+        this.tipPercentage = 15.0;
+        this.bill = 0.0;
+    }
+
+    /**
+     * Gets the number of guests.
+     *
+     * @return the number of guests
+     */
+    public int getGuests() {
+        return guests;
+    }
+
+    /**
+     * Sets the number of guests.
+     *
+     * @param guests the number guests
+     */
+    public void setGuests(int guests) {
+        this.guests = guests;
+    }
+
+    /**
+     * Gets the tip percentage.
+     *
+     * @return the tip percentage
+     */
+    public double getTipPercentage() {
+        return tipPercentage;
+    }
+
+    /**
+     * Sets the tip percentage.
+     *
+     * @param tipPercentage the tip percentage
+     */
+    public void setTipPercentage(double tipPercentage) {
+        this.tipPercentage = tipPercentage;
+    }
+
+    /**
+     * Gets the initial bill.
+     *
+     * @return the bill
+     */
+    public double getBill() {
+        return bill;
+    }
+
+    /**
+     * Sets what the bill is before tip.
+     *
+     * @param bill the bill
+     */
+    public void setBill(double bill) {
+        this.bill = bill;
+    }
+
+    /**
+     * Gets total tip for the bill and tip percentage.
+     *
+     * @return the total tip
+     */
+    public double getTotalTip() {
+        return totalTip;
+    }
+
+
+    /**
+     * Gets total bill including tip.
+     *
+     * @return the total bill
+     */
+    public double getTotalBill() {
+        return totalBill;
+    }
+
+    /**
+     * Gets tip per guest.
+     *
+     * @return the tip per guest
+     */
+    public double getTipPerGuest() {
+        return tipPerGuest;
+    }
+
+
+    /**
+     * Gets total per guest.
+     *
+     * @return the total per guest
+     */
+    public double getTotalPerGuest() {
+        return totalPerGuest;
+    }
+
+}
