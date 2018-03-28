058e4ac5b44227bd0b1a39606dc4983ba6b132b7
EthanRhodes <erhodes7@wvup.edu>
2018-03-27 22:47:28 -0400

    i lied this is the final push
    puuuush

---
+++ Chapter9/app/src/main/java/com/example/ethan/chapter9/InputFragment.java
@@ -0,0 +1,22 @@
+package com.example.ethan.chapter9;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class InputFragment extends Fragment
+{
+    public InputFragment()
+    {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        return inflater.inflate( R.layout.fragment_bottom_input, container, false );
+    }
+
+}
