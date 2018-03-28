db24d16826e4d296119521cbdcb56893f4b1f9e7
EthanRhodes <erhodes7@wvup.edu>
2018-03-27 22:28:24 -0400

    ch4ng3s
    qsewqewq

--- Chapter9/app/src/main/java/com/example/ethan/chapter9/MainActivity.java
+++ Chapter9/app/src/main/java/com/example/ethan/chapter9/MainActivity.java
@@ -3,11 +3,17 @@ package com.example.ethan.chapter9;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+
+    private RandomNumFragment numb;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 }
+
+
---
+++ Chapter9/app/src/main/java/com/example/ethan/chapter9/RngModel.java
@@ -0,0 +1,42 @@
+package com.example.ethan.chapter9;
+
+public class RngModel
+{
+    int randomNumber;
+
+    public RngModel(int randomNumber)
+    {
+        setRandomNumb(randomNumber);
+    }
+
+    public void setRandomNumb(int number)
+    {
+        randomNumber = number;
+    }
+
+
+    public String howClose(int guess)
+    {
+        if(guess >= 1 ||  guess <= 100)
+        {
+            if(guess == randomNumber)
+            {
+                return "Winner winner, Chicken Dinner! You got it!";
+            }
+            else if (guess >=  randomNumber)
+            {
+                return "Yikes, Too low!";
+            }
+            else if(guess <= randomNumber)
+            {
+                return "Too High Brother!";
+            }
+        }
+        return "Not even close bro!";
+    }
+
+
+
+
+}
+
