6f55ce8ee08601109b13f3e4ab1c08d1ef5a5cfc
EthanRhodes <erhodes7@wvup.edu>
2018-03-27 20:08:04 -0400

    yikes
    yikes brother bo

---
+++ Chapter9/app/src/main/java/com/example/ethan/chapter9/PromptFragment.java
@@ -0,0 +1,21 @@
+package com.example.ethan.chapter9;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class PromptFragment extends Fragment
+{
+    public PromptFragment()
+    {
+
+    }
+
+    @Override
+    public View onCreateView( LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        return inflater.inflate(R.layout.fragment_top_prompt, container, false);
+    }
+}
